01a310a62d66f9be653a08a90b3d1ecc0a97e5d0
Vivian Gu <vgu6@gatech.edu>
2025-01-16 15:53:37 -0500

    gaze interaction working

---
+++ Assets/Scripts/InfoBehavior.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfoBehavior : MonoBehaviour
+{
+    const float SPEED = 6f;
+
+    [SerializeField]
+    Transform sectionInfo;
+
+    Vector3 desiredScale = Vector3.zero;
+
+    // Update is called once per frame
+    void Update()
+    {
+        sectionInfo.localScale = Vector3.Lerp(sectionInfo.localScale, desiredScale, Time.deltaTime * SPEED);
+    }
+
+    public void OpenInfo() {
+        desiredScale = Vector3.one;
+    }
+
+    public void CloseInfo() {
+        desiredScale = Vector3.zero;
+    }
+}
